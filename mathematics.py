bec1b1eaefa964af4fae89837e9bfe2dba5bdf0f
Vladislav Kuznetsov <starforce882@gmail.com>
2021-07-27 02:04:35 +0300

    Renamed & improved math.py

---
+++ mathematics.py
@@ -0,0 +1,38 @@
+# математика для самых маленьких
+
+a = 5
+b = 6
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a**b)
+
+# TODO: калькулятор
+try:
+    a=int(input('Первое число:'))
+    b=int(input('Второе число:'))
+    znak=input('Что с ними сделать?')
+    res="Ваш ответ: "
+except(ValueError):
+    print("Ошибка: введите число")
+else:
+    if znak == "":
+        print('Ошибка: введите действие, которое хотите совершить. На выбор:"+", "-", "*" и "/"')
+    try:
+        if znak == "+":
+            print(res, a+b)
+        elif znak == "-":
+            print(res, a-b)
+        elif znak == "*":
+            print(res, a*b)
+        elif znak == "/":
+            print(res, a/b)
+    except(ZeroDivisionError):
+        print("Ошибка: нельзя делить на ноль.")
+
