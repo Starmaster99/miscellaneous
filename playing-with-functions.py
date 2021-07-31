e9ee054ab69a9c75876757eb946aa1dfbf4a5f85
Vladislav Kuznetsov <starforce882@gmail.com>
2021-08-01 01:00:19 +0300

    Trying to learn something

---
+++ playing-with-functions.py
@@ -0,0 +1,35 @@
+# функции для самых маленьких
+
+user_list = []
+
+try:
+    print("Назови мне список чисел, и я посчитаю их сумму!")
+    n = input("Сколько их будет всего? ").strip()
+    n = int(n)
+    print("А теперь назови их самих")
+    for n in range(0, n):
+        user_var = int(input())
+        user_list.append(user_var)
+
+except ValueError:
+    print("Писать можно только цифры!")
+
+
+def main(user_list):
+    return sum(user_list)
+
+
+a = main(user_list)
+print(f"Сумма ваших чисел составляет {a}!")
+
+
+def hello():
+    name = input("Как мне тебя называть? ")
+    return name
+
+def hi(name):
+    print(f"Приветствую, {name}!")
+
+hi(hello())
+
+
